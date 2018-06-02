0567d5da58f2a51fa6cde92e3353fbba6c6a9fe9
qiushaoxi <qiushaoxi@uchain.one>
2018-06-03 04:04:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,128 @@
+const ipfsAPI = require('ipfs-api');
+const readline = require('readline');
+const superagent = require('superagent');
+const fs = require('fs');
+
+var rl1 = readline.createInterface({
+    input: fs.createReadStream('snapshot.csv')
+});
+var valid = 0;
+var invalid = 0;
+var promises = [];
+rl1.on('line', (line) => {
+    var arr = line.toString().split(",");
+    let account = arr[1].replace(/\"/g, "");
+    let snapshotPublicKey = arr[2].replace(/\"/g, "");
+    let snapshotBalance = arr[3].replace(/\"/g, "");
+    promises.push(
+        validate(account, snapshotBalance, snapshotPublicKey)
+            .then((res) => {
+                console.log(account, res);
+                //valid += 1;
+                return;
+            }).catch((err) => {
+                console.error(account, err);
+                //invalid += 1;
+                return;
+            })
+    )
+});
+
+var ipfs = ipfsAPI('localhost', '5001', { protocol: 'http' }) // leaving out the arguments will default to these values
+
+const validCID = 'QmYr3VPdxARc5RVSrQjYpzCMsBjKVkBrFNGjdATsQisyb6'
+
+const httpEndPoint = "127.0.0.1:10999";
+
+const stream = ipfs.files.getReadableStream(validCID)
+
+stream.on('data', (file) => {
+    // write the file's path and contents to standard out
+    console.log(file.path)
+    if (file.type !== 'dir') {
+        const rl = readline.createInterface({
+            input: file.content
+        });
+        rl.on('line', (line) => {
+            var arr = line.toString().split(",");
+            let account = arr[1].replace(/\"/g, "");
+            let snapshotPublicKey = arr[2].replace(/\"/g, "");
+            let snapshotBalance = arr[3].replace(/\"/g, "");
+            promises.push(
+                validate(account, snapshotBalance, snapshotPublicKey)
+                    .then((res) => {
+                        console.log(account, res);
+                        //valid += 1;
+                        return;
+                    }).catch((err) => {
+                        console.error(account, err);
+                        //invalid += 1;
+                        return;
+                    })
+            )
+        })
+    }
+});
+
+
+function validate(account, snapshotBalance, snapshotPublicKey) {
+    //let vaild = true;
+    snapshotBalance = snapshotBalance.toString().split(" ")[0];
+    return new Promise((resolve, reject) => {
+        superagent(httpEndPoint + "/v1/chain/get_currency_balance")
+            .set('Content-Type', 'application/json')
+            .send({
+                "code": "eosio.token",
+                "account": account,
+                "symbol": "EOS"
+            })
+            .end(function (err, res) {
+                if (err) {
+                    console.error("http error :" + err);
+                } else if (res.statusCode != 200) {
+                    console.error("status code :" + res.statusCode);
+                    return;
+                } else {
+                    //console.log(res.text);
+                    let balance = JSON.parse(res.text)[0];
+                    balance = 1 * balance.toString().split(" ")[0];
+                    superagent(httpEndPoint + "/v1/chain/get_account")
+                        .set('Content-Type', 'application/json')
+                        .send({
+                            "account_name": account,
+                        })
+                        .end(function (err, res) {
+                            if (err) {
+                                console.error("http error :" + err);
+                            } else if (res.statusCode != 200) {
+                                console.error("status code :" + res.statusCode);
+                                return;
+                            } else {
+                                let object = JSON.parse(res.text);
+                                let stake_cpu = 1 * object.total_resources.cpu_weight.toString().split(" ")[0];;
+                                let stake_net = 1 * object.total_resources.net_weight.toString().split(" ")[0];;
+                                let ram_bytes = object.total_resources.ram_bytes;
+                                let owner_key = object.permissions[0].required_auth.keys[0].key;
+                                let active_key = object.permissions[1].required_auth.keys[0].key;
+                                if (owner_key != snapshotPublicKey || active_key != snapshotPublicKey) {
+                                    //console.error("snapshotPublicKey error");
+                                    reject("snapshotPublicKey error,snapshot:" + snapshotPublicKey + ",owner:" + owner_key + ",active:" + active_key);
+                                }
+                                if (ram_bytes > 8192) {
+                                    //console.error("ram error");
+                                    reject("ram error,ram:" + ram_bytes);
+                                }
+                                let total = balance + stake_cpu + stake_net;
+                                if (total != snapshotBalance) {
+                                    //console.error("balance error");
+                                    reject("balance error,snapshot:" + snapshotBalance + ",balance:" + balance + ",stake_cpu:" + stake_cpu + ",stake_net:" + stake_net);
+                                }
+                                resolve(":ok.");
+                            }
+                        }
+                        )
+                }
+            });
+    });
+
+}
